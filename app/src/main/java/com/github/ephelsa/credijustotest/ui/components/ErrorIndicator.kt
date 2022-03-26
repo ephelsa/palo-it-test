6e3aae8272e58a8ecac00b9bcd5f2edb18504da3
Leonardo <xephelsax@gmail.com>
2022-03-25 23:20:08 -0500

    ✨: Create an error view indicator

---
+++ app/src/main/java/com/github/ephelsa/credijustotest/ui/components/ErrorIndicator.kt
@@ -0,0 +1,41 @@
+package com.github.ephelsa.credijustotest.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.github.ephelsa.credijustotest.R
+import com.github.ephelsa.credijustotest.ui.theme.LightGray
+
+/**
+ * Error composable to display when something unexpected occurs interacting with data.
+ */
+@Composable
+fun ErrorIndicator() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Icon(
+            modifier = Modifier.size(80.dp),
+            imageVector = Icons.Rounded.Close,
+            contentDescription = stringResource(id = R.string.contentDescription_errorIndicator),
+            tint = LightGray
+        )
+        Text(
+            text = stringResource(id = R.string.contentDescription_errorIndicator),
+            color = LightGray,
+        )
+    }
+}
